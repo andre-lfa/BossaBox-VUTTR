a07d7d50c3728db8095c142f0c4289792ea142d8
Andre LFA <andrelfaareas@gmail.com>
2020-11-11 11:36:17 -0300

    Configura a criação de migrations

---
+++ src/db/migrations/1605105319143-create_tools.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class createTools1605105319143 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
